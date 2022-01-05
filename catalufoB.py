47ec0b89661282019c6a3a8ed154541a218d0c0b
Antonio Villanueva Segura <karpvs@yahoo.fr>
2022-01-05 12:45:18 +0100

    Update catalufoB.py
    Un analisis ligero de terminaciones verbales

--- catalufoB.py
+++ catalufoB.py
@@ -7,15 +7,15 @@ https://paraulogic.rodamots.cat/
 
 def limpiar_acentos(text):
 	""" elimina acentos de un string """
-	acentos = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'Á': 'A', 'E': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U'}
+	acentos = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'Á': 'A',
+				'E': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U'}
 	for acen in acentos:
 		if acen in text:
 			text = text.replace(acen, acentos[acen])
 	return text
 	
-
 	
-def testLineaB ( letras,obligatoria,linea):
+def testLinea ( letras,obligatoria,linea):
 	""" mira si una palabra del diccionario contiene algunas  letras minimo 3 """
 
 	#print (letras,"con ",limpiar_acentos(linea), "longitud linea ",len (linea))
@@ -28,28 +28,40 @@ def testLineaB ( letras,obligatoria,linea):
 	
 	#Si la palabra contiene minimo 3 ocurrencias es aceptada	
 	if ( (contiene >=3)   and ( len(linea)== contiene)   ) :	
-		return True
-			
-
+		return True			
 	return False
 	
+def posibleVerbo (palabra):
+	""" Analiza posibles terminaciones verbales """	
+	with open ("verbos.txt", encoding="latin-1") as verbos:
+		#Recorre algunas terminaciones verbales
+
+		for verbo in verbos:
+			#print (palabra,"  ",verbo,"  ",palabra.rfind(verbo.strip()),end=" ")	
+			#Si -1 ha sido encontrada la terminacion 		
+			if  (palabra.rfind(verbo.strip()) != -1):
+				return " ? "
+				
+	return " - "
+				
 
 if __name__ == "__main__":
 
 	letras = input ("Introduce las letras ")
-	especial =input ("Introduce la letra obligatoria ")
+	especial =input ("Introduce la letra central obligatoria ")
 
 	#letras="roeign"
 	#obligatoria="d"
-	""" unicode(str, errors='ignore') """
+
 	with open("catala.dic", encoding="latin-1") as fname:
-		print ("Diccionario abierto ")
-		""" lee lineas"""
+		# recorre las palabras del diccionario por lineas
 		for linea in fname:
 			
-			#if testLinea(letras.strip(),linea.strip()):
-			if testLineaB(letras.strip(),obligatoria,linea.split("/")[0]):		
-	
+			if testLinea(letras.strip(),obligatoria,linea.split("/")[0]):		
+
 				if obligatoria  in linea:
-					print(linea.split("/")[0])
+						
+					print(linea.split("/")[0],end="")
+					print (posibleVerbo(linea.split("/")[0]))
 	
+
